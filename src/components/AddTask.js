bf4f2f29c9e5c4a139745772e84442b78254b6e1
Marsel Gray <marselgray@gmail.com>
2021-10-13 14:19:18 -0400

    Build: add task form

---
+++ src/components/AddTask.js
@@ -0,0 +1,25 @@
+const AddTask = () => {
+    return (
+        <form classsName="add-form">
+
+            <div className="form-control">
+                <label>Task</label>
+                <input type="text" placeholder="Add Task" />
+            </div>
+
+            <div className="form-control">
+                <label>Day & Time</label>
+                <input type="text" placeholder="Add Day & Time" />
+            </div>
+
+            <div className="form-control form-control-check">
+                <label>Set Reminder</label>
+                <input type="checkbox" />
+            </div>
+
+            <input type="submit" value="Save Task" className="btn btn-block" />
+        </form>
+    )
+}
+
+export default AddTask
