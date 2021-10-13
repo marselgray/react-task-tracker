912de6101de70fe3755c4fe3eb3f175f868d9e88
Marsel Gray <marselgray@gmail.com>
2021-10-13 14:05:30 -0400

    Build: task toggle UI

--- src/App.js
+++ src/App.js
@@ -34,11 +34,18 @@ const App = () => {
     id))
   }
 
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    // console.log(id);
+    setTasks(tasks.map((task) => 
+      task.id === id ? {...task, reminder: !task.reminder} : task )
+    )
+  }
+
   return (
     <div className="container">
         < Header />
-        {tasks.length > 0 ? < Tasks tasks={tasks} onDelete={deleteTask} /> : 'No Tasks'}
-        
+        {tasks.length > 0 ? < Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks'}
     </div>
   );
 }
