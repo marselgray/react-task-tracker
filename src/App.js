8ed582e9cad79340783711b3a87b5ff3cd4e8b6e
Marsel Gray <marselgray@gmail.com>
2021-10-13 13:07:01 -0400

    Build: no task message

--- src/App.js
+++ src/App.js
@@ -27,10 +27,18 @@ const App = () => {
     ]
   )
 
+  // Delete Task
+  const deleteTask = (id) => {
+    // console.log('delete', id);
+    setTasks(tasks.filter((task) => task.id !== 
+    id))
+  }
+
   return (
     <div className="container">
         < Header />
-        < Tasks tasks={tasks} />
+        {tasks.length > 0 ? < Tasks tasks={tasks} onDelete={deleteTask} /> : 'No Tasks'}
+        
     </div>
   );
 }
